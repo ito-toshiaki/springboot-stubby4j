4093e6763f87d1490e1f0f703da7023df3bdc797
Toshiaki Ito <evolution@1024kb.cx>
2017-11-11 20:19:59 +0900

    add comments.

--- src/main/java/cx/mb/stubby4j/Stubby4jApplication.java
+++ src/main/java/cx/mb/stubby4j/Stubby4jApplication.java
@@ -3,9 +3,16 @@ package cx.mb.stubby4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class.
+ */
 @SpringBootApplication
 public class Stubby4jApplication {
 
+	/**
+	 * Main method.
+	 * @param args args.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Stubby4jApplication.class, args);
 	}
